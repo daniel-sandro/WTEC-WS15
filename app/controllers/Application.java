0c3d6bf20848e0df03e23afdbb9bf6eee00635de
dbarelop <dbarelop@gmail.com>
2015-11-04 11:35:15 +0100

    Added example navbar with home, rules and about pages using Bootstrap

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,23 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.about;
+import views.html.index;
+import views.html.rules;
 
 public class Application extends Controller {
 
     public Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render(""));
+    }
+
+    public Result rules() {
+        return ok(rules.render());
+    }
+
+    public Result about() {
+        return ok(about.render());
     }
 
 }
