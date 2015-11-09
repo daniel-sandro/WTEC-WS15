cd8371d6d6499fcbb378a6656bc7f485c5e210f8
dbarelop <dbarelop@gmail.com>
2015-11-10 00:47:30 +0100

    Implemented login

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,16 +4,23 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import de.htwg.battleship.BattleshipModule;
 import de.htwg.battleship.controller.IController;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.about;
-import views.html.battleship;
-import views.html.index;
-import views.html.rules;
+import views.html.*;
 
 public class Application extends Controller {
 
     public Result index() {
+        return ok(index.render(""));
+    }
+
+    public Result login() {
+        return ok(login.render(play.data.Form.form(Login.class)));
+    }
+
+    public Result logout() {
+        session().clear();
         return ok(index.render(""));
     }
 
@@ -30,6 +37,27 @@ public class Application extends Controller {
         IController controller = injector.getInstance(IController.class);
 
         return ok(battleship.render(10, "10%", "10%"));
+    }
+
+    public Result authenticate() {
+        Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
+        if (loginForm.hasErrors()) {
+            return badRequest(login.render(loginForm));
+        } else {
+            session().clear();
+            session("email", loginForm.get().email);
+            return redirect(routes.Application.index());
+        }
+    }
+
+    public String validate() {
+        // TODO: implement
+        return null;
+    }
+
+    public static class Login {
+        public String email;
+        public String password;
     }
 
 }
