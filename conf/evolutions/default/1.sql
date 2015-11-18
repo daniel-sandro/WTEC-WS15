91170ef90c84ea74976bf9d13475af32d6d98eeb
dbarelop <dbarelop@gmail.com>
2015-11-18 18:19:59 +0100

    Fixed dependencies for gulp

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -24,20 +24,16 @@ create sequence linked_account_seq;
 
 create sequence users_seq;
 
-alter table linked_account add constraint fk_linked_account_user_1 foreign key (user_id) references users (id) on delete restrict on update restrict;
+alter table linked_account add constraint fk_linked_account_user_1 foreign key (user_id) references users (id);
 create index ix_linked_account_user_1 on linked_account (user_id);
 
 
 
 # --- !Downs
 
-SET REFERENTIAL_INTEGRITY FALSE;
+drop table if exists linked_account cascade;
 
-drop table if exists linked_account;
-
-drop table if exists users;
-
-SET REFERENTIAL_INTEGRITY TRUE;
+drop table if exists users cascade;
 
 drop sequence if exists linked_account_seq;
 
