c57a1e8752c915df7fc2b35b57808343b40d8274
dbarelop <dbarelop@gmail.com>
2015-11-18 16:54:39 +0100

    Implemented user service

--- app/service/BattleshipUserService.java
+++ app/service/BattleshipUserService.java
@@ -4,10 +4,10 @@ import com.feth.play.module.pa.service.UserServicePlugin;
 import com.feth.play.module.pa.user.AuthUser;
 import com.feth.play.module.pa.user.AuthUserIdentity;
 import com.google.inject.Inject;
+import models.User2;
 import play.Application;
 
 public class BattleshipUserService extends UserServicePlugin {
-    // TODO: implement
 
     @Inject
     public BattleshipUserService(Application app) {
@@ -16,21 +16,27 @@ public class BattleshipUserService extends UserServicePlugin {
 
     @Override
     public Object save(AuthUser authUser) {
-        return null;
+        final boolean isLinked = User2.existsByAuthUserIdentity(authUser);
+        return isLinked ? null : User2.create(authUser).id;
     }
 
     @Override
     public Object getLocalIdentity(AuthUserIdentity identity) {
-        return null;
+        final User2 u = User2.findByAuthUserIdentity(identity);
+        return u == null ? null : u.id;
     }
 
     @Override
     public AuthUser merge(AuthUser newUser, AuthUser oldUser) {
-        return null;
+        if (!oldUser.equals(newUser)) {
+            User2.merge(oldUser, newUser);
+        }
+        return oldUser;
     }
 
     @Override
     public AuthUser link(AuthUser oldUser, AuthUser newUser) {
+        User2.addLinkedAccount(oldUser, newUser);
         return null;
     }
 }
