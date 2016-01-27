4f1808de359cc1d051b67a4ceb34871e1e4b419c
dbarelop <dbarelop@gmail.com>
2016-01-27 01:45:11 +0100

    Added chart to profile page (implemented in AngularJS)

--- public/javascripts/dist/angular.js
+++
@@ -1,9 +0,0 @@
-var myApp = angular.module('myApp', []);
-
-myApp.directive('profileApp', function() {
-    return {
-        restrict: 'AECM',
-        replace: true,
-        templateUrl: '../../elements/profile.html'
-    };
-});
